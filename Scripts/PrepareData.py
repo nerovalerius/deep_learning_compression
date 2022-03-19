c8fdcbceed07e9a46189dd64a67b2d08657af5f1
simonschindler <simon.schindler97@gmx.de>
2022-03-19 12:45:16 +0100

    small workaround fixing unability of imports

--- Scripts/PrepareData.py
+++ Scripts/PrepareData.py
@@ -1,3 +1,15 @@
+import sys
+from pathlib import Path
+
+# TODO
+# This is just a workaround, because python in my WSL
+# adds the folder of the script to sys.path instead
+# of the workspace folder of vscode, which is cwd, when
+# you run or debug a script in vscode.
+# We should resolve this by using a venv.
+root = Path("./")
+sys.path.insert(0, str(root.resolve()))
+
 from deep_learning_compression.Storage.Storage import Storage
 
 if __name__ == "__main__":
